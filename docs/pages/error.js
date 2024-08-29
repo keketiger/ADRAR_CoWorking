626b5982cd9fb2a8405fc94077b76d524d72093f
keketiger <34973659+keketiger@users.noreply.github.com>
2024-08-29 15:04:56 +0200

    Fixing the display of the navigation bar for phones

--- docs/pages/error.js
+++ docs/pages/error.js
@@ -3,7 +3,7 @@ function render() {
         <div style="text-align: center; margin-top: 5vh;">
             <img src="assets/medias/error.png" alt="error-logo" style="width: 250px; height: auto;">
             <h2>Erreur 404</h2>
-            <p>Oupss, la page n'a pu être trouvé !</p>
+            <p>Oupss, la page n'a pas pu être trouvée !</p>
         </div>
     `;
 }
