43b7c6d43e839f6195acb737d40ac53c33fc2943
keketiger <34973659+keketiger@users.noreply.github.com>
2024-08-29 14:18:53 +0200

    Improve code & add error page

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,3 +1,6 @@
+const loadedScripts = new Set();
+const renderCache = new Map();
+
 const categories = [
     {
         title: 'Accueil', icon: 'fas fa-home',
@@ -22,15 +25,24 @@ const categories = [
             {title: "Création d'un Client", page: 'windows/client_setup'},
             {title: "Création d'un Active Directory", page: 'windows/active_directory'},
         ]
+    },
+    {
+        title: 'Cisco Packet Tracer', icon: 'fa-solid fa-network-wired',
+        subcategories: [
+            {title: "Configurer un routeur", page: 'cisco_packet/config_router'},
+            {title: "Configurer un switch", page: 'cisco_packet/config_switch'},
+            {title: "Mise en place du SSH", page: 'cisco_packet/setup_ssh'},
+            {title: "Mise en place des ACL", page: 'cisco_packet/setup_acl'}
+        ]
     }
 ];
 
-function generateSideBar() {
+const generateSideBar = () => {
     const sidebar = document.querySelector('.sidebar .menu');
     sidebar.innerHTML = categories.map((category, index) => `
         <li>
             <button class="menu-btn" onclick="handleMenuClick(this, '${category.page}', ${!!category.subcategories}, 'submenu${index}')"
-                ${category.page === 'home' ? 'data-active\="true"' : ''}>
+                ${category.page === 'home' ? 'data-active="true"' : ''}>
                 <i class="${category.icon}"></i> ${category.title}
             </button>
             ${category.subcategories ? `
@@ -43,7 +55,14 @@ function generateSideBar() {
     `).join('');
 }
 
-function handleMenuClick(button, page, hasSubmenu, submenuId) {
+const toggleSubmenu = (submenuId) => {
+    document.querySelectorAll('.submenu').forEach(submenu => {
+        submenu.style.display = submenu.id === submenuId ?
+            (submenu.style.display === 'block' ? 'none' : 'block') : 'none';
+    });
+}
+
+const handleMenuClick = (button, page, hasSubmenu, submenuId) => {
     document.querySelectorAll('.menu-btn').forEach(btn => btn.removeAttribute('data-active'));
     button.setAttribute('data-active', 'true');
     if (hasSubmenu) {
@@ -53,53 +72,46 @@ function handleMenuClick(button, page, hasSubmenu, submenuId) {
     }
 }
 
-function toggleSubmenu(submenuId) {
-    document.querySelectorAll('.submenu').forEach(submenu => {
-        submenu.style.display = submenu.id === submenuId ?
-            (submenu.style.display === 'block' ? 'none' : 'block') : 'none';
-    });
+const hideAllSubmenus = () => document.querySelectorAll('.submenu').forEach(submenu => submenu.style.display = 'none');
+
+const loadScript = (scriptSrc, onload, onerror) => {
+    if (loadedScripts.has(scriptSrc)) return onload();
+
+    const script = document.createElement('script');
+    script.src = scriptSrc;
+    script.onload = onload;
+    script.onerror = onerror;
+    document.body.appendChild(script);
+    loadedScripts.add(scriptSrc);
 }
 
-function hideAllSubmenus() {
-    document.querySelectorAll('.submenu').forEach(submenu => submenu.style.display = 'none');
-}
-
-const loadedScripts = new Set();
-const renderCache = new Map();
-
-function renderPage(page) {
+const renderPage = (page, content) => {
     const pageContent = window['render']();
     renderCache.set(page, pageContent);
-    const content = document.getElementById('content');
     content.innerHTML = pageContent;
     content.className = 'fade-in';
-}
+};
 
-function loadPage(page) {
+const loadErrorPage = content => loadScript('pages/error.js', () => renderPage('error', content));
+
+const loadPage = (page) => {
+    const content = document.getElementById('content');
+
     if (page === 'home') hideAllSubmenus();
 
-    const content = document.getElementById('content');
     content.className = 'fade-out';
     content.innerHTML = "<p>Chargement...</p>";
 
     if (renderCache.has(page)) {
         content.innerHTML = renderCache.get(page);
         content.className = 'fade-in';
-        return;
-    }
-
-    const scriptSrc = `pages/${page}.js`;
-
-    if (loadedScripts.has(scriptSrc)) {
-        renderPage(page);
     } else {
-        const script = document.createElement('script');
-        script.src = scriptSrc;
-        script.onload = () => renderPage(page);
-        document.body.appendChild(script);
-        loadedScripts.add(scriptSrc);
+        loadScript(`pages/${page}.js`,
+            () => renderPage(page, content),
+            () => loadErrorPage(content)
+        );
     }
-}
+};
 
 document.addEventListener('DOMContentLoaded', () => {
     generateSideBar();
---
+++ docs/pages/error.js
@@ -0,0 +1,9 @@
+function render() {
+    return `
+        <div style="text-align: center; margin-top: 5vh;">
+            <img src="assets/medias/error.png" alt="error-logo" style="width: 250px; height: auto;">
+            <h2>Erreur 404</h2>
+            <p>Oupss, la page n'a pu être trouvé !</p>
+        </div>
+    `;
+}
